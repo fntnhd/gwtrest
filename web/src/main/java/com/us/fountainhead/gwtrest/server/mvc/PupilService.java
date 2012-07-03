421c1132216b39a17d7bf9d208dbdc92894ed46f
Steve Andrews <steve@fountainhead.us.com>
2012-07-03 15:09:42 -0400

    Changed return type for service operations.

--- web/src/main/java/com/us/fountainhead/gwtrest/server/mvc/PupilService.java
+++ web/src/main/java/com/us/fountainhead/gwtrest/server/mvc/PupilService.java
@@ -4,15 +4,9 @@ import com.us.fountainhead.gwtrest.server.entity.Pupil;
 import com.us.fountainhead.gwtrest.server.entity.PupilDAO;
 import com.us.fountainhead.gwtrest.server.validate.ValidationException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -27,7 +21,7 @@ public class PupilService {
     @RequestMapping(method = RequestMethod.GET, value = "/pupil/findById/{id}", headers = "Accept=application/json")
     public @ResponseBody
     FindByIdResponse findById(@PathVariable Long id) {
-        FindByIdResponse response = null;
+        FindByIdResponse response = new FindByIdResponse();
 
         try {
             response.setPupil(dao.findById(id));
@@ -107,32 +101,54 @@ public class PupilService {
 
     @RequestMapping(method = RequestMethod.PUT, value = "/pupil/update", headers = "Accept=application/json")
     public @ResponseBody
-    Pupil update(@RequestBody Pupil pupil) {
-        Pupil response = null;
+    UpdateResponse update(@RequestBody Pupil pupil) {
+        UpdateResponse response = new UpdateResponse();
 
         try {
-            response = dao.update(pupil);
-
-
+            response.setPupil(dao.update(pupil));
         } catch (ValidationException ex) {
-            Logger.getLogger(PupilService.class.getName()).log(Level.SEVERE, null, ex);
+            response.setErrorList(ex.getErrors());
         }
 
         return response;
     }
 
+    public class UpdateResponse extends ServiceResponse {
+
+        private Pupil pupil;
+
+        public void setPupil(Pupil pupil) {
+            this.pupil = pupil;
+        }
+
+        public Pupil getPupil() {
+            return pupil;
+        }
+    }
+
     @RequestMapping(method = RequestMethod.DELETE, value = "/pupil/delete/{id}", headers = "Accept=application/json")
     public @ResponseBody
-    Pupil delete(@PathVariable Long id) {
-        Pupil response = null;
+    DeleteResponse delete(@PathVariable Long id) {
+        DeleteResponse response = new DeleteResponse();
         try {
-            response = dao.delete(id);
-
-
+            response.setPupil(dao.delete(id));
         } catch (ValidationException ex) {
-            Logger.getLogger(PupilService.class.getName()).log(Level.SEVERE, null, ex);
+            response.setErrorList(ex.getErrors());
         }
 
         return response;
+    }
+
+    public class DeleteResponse extends ServiceResponse {
+
+        private Pupil pupil;
+
+        public void setPupil(Pupil pupil) {
+            this.pupil = pupil;
+        }
+
+        public Pupil getPupil() {
+            return pupil;
+        }
     }
 }
